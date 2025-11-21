e83e267457d45abb21503f84b8f9be26212ccacf
Shyampatil07 <shyamlade06@gmail.com>
2025-11-21 22:12:37 +0530

    Add Project Files

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -19,9 +19,10 @@ export default function WeatherApp(){
     };
 
     return(
-        <div style={{textAlign:"center"}}>
+        <div style={{ textAlign: "center", marginLeft: "600px" }}>
             <h2>Weather App by Delta</h2>
             <SearchBox updateInfo={updateInfo}/>
+            <br></br>
             <InfoBox info={weatherInfo}/>
         </div>
     );
